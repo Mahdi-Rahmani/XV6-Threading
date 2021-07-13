61f773411d45bcddcf7844e54eeea700cf7ea3a7
mahdi <mahdi.rezaie.336@gmail.com>
2021-07-13 17:07:54 +0430

    Copy sourc code

---
+++ cmake-build-debug/CMakeFiles/XV6.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/XV6.dir/main.c.o"
+  "XV6"
+  "XV6.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/XV6.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
