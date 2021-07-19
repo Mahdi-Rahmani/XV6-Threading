51b0539136cfc2a1aba776d8ebc884d6bdef40e0
mahdi <mahdi.rezaie.336@gmail.com>
2021-07-20 03:26:20 +0430

    Change testmthread to print increasing x example

--- testmthread.c
+++ testmthread.c
@@ -3,27 +3,25 @@
 #include "user.h"
 #include "mthread.c"
 
-int arrsum = 0;
-int arr[] = {1, 2, 3, 4, 5, 6}; // 1 + 2 + 3 + 4 + 5 + 6 = 21
+int x = 0;
 
-void sum(void *x)
+void sum(void *y)
 {
-    arrsum += *((int *)x);
-    return;
+    for(;;) {
+      x++;
+      sleep(100);
+    }
 }
 
 int main()
 {
-    for(int i = 0; i < 6; i++)
-    {
-        thread_create(&sum, (void *)&arr[i]);
+    int arr[] = {1};
+    thread_create(&sum, (void *)arr);
+    for(;;) {
+        x++;
+      printf(1, "x = %d\n", x);
+      sleep(100);
     }
-    for(int i = 0; i < 6; i++)
-    {
-        thread_join();
-    }
-    printf(1, "**************");
-    printf(1,"*%d\n", arrsum);
 
     exit(); 
 }
